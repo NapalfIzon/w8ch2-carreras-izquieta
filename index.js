cb5955259c99124f67fa3c1d59a17cc223100e18
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-17 18:41:17 +0100

    :memo: Adding to root index.js the initializer of the monDB connection

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const initializeServer = require("./server");
-
 require("dotenv").config();
+const initializeMongoDb = require("./database");
+const initializeServer = require("./server");
 
 const port = process.env.PORT || process.env.LOCAL_PORT || 8000;
 
 (async () => {
   await initializeServer(port);
+  await initializeMongoDb(process.env.MONGODB_STRING);
 })();
